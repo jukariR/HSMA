908b7a08c43d0651579a21722b6c1bb164f2835b
Jukari <jukari98@gmail.com>
2022-11-27 17:36:50 -0600

    Crud service changed from login component to service.ts

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, HostListener } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
 import {Guests} from 'src/app/interfaces/guests';
+import { CrudService } from 'src/app/services/crud.service';
 
 @Component({
   selector: 'app-login',
@@ -14,13 +13,13 @@ export class LoginComponent implements OnInit {
   width: number = 0;
   public changeClass = false;
   
-  constructor(private firestore: AngularFirestore) {
+  constructor(private crudService: CrudService) {
     this.items = [];
-    this.firestore.collection('guests').valueChanges({ name: '', room: 0 }).subscribe((users: any) => {
-      this.items = users;
-      console.log("get data", this.items[0]);
-      console.log("firebase data", users);
-    });
+    // this.firestore.collection('guests').valueChanges({ name: '', room: 0 }).subscribe((users: any) => {
+    //   this.items = users;
+    //   console.log("get data", this.items[0]);
+    //   console.log("firebase data", users);
+    // });
   }
 
   ngOnInit(): void {
@@ -28,6 +27,8 @@ export class LoginComponent implements OnInit {
   }
 
   async info(){
+    this.crudService.getGuests(this.items);
+    console.log(this.items);
     // this.items = await this.firestore.collection('guests').valueChanges();
     // console.log(this.items);
   }
--- src/app/services/crud.service.ts
+++ src/app/services/crud.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Guests } from '../interfaces/guests';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore) { }
+
+  getGuests(items: Guests[]) {
+    this.firestore.collection('guests').valueChanges({ name: '', room: 0 }).subscribe((users: any) => {
+      items = users;
+      console.log("get data", items[0]);
+      console.log("firebase data", users);
+    });
+  }
+
 }
